31e679ba744d41af03178f519abdd5ba6a4d1de3
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-13 13:52:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(todo = 'roller-skate'){
+    return `This Saturday, I want to ${todo}!`
+}
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+    }
+function wrapAdjective(wrap = "*"){
+    return function (adjective){
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+    }
+
+
